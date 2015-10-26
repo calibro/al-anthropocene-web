4dac667ac0bfdbe5d88b9c589efc95f576fefd98
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2015-10-26 16:22:29 +0100

    added reset on network

--- app/scripts/controllers/network.js
+++ app/scripts/controllers/network.js
@@ -8,11 +8,15 @@
  * Controller of the anthropoceneWebApp
  */
 angular.module('anthropoceneWebApp')
-  .controller('NetworkCtrl', function ($scope, networkData, socket) {
+  .controller('NetworkCtrl', function ($scope, networkData, socket,$window) {
     $scope.netData = networkData;
     $scope.nodeSelected = [];
 
     socket.on('entities', function(data){
       $scope.nodeSelected = data.entities;
     });
+
+    socket.on("resetCreate",function(data){
+      $window.location.reload();
+    })
   });
